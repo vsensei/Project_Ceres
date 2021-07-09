a2dfe9f3547ec021413443fceb6418a5dec281d2
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-09 06:38:46 +0300

    added section dummy

---
+++ client/src/dummies/sectionsDummy.js
@@ -0,0 +1,41 @@
+const sections = [
+  {
+    id: 1,
+    title: 'pasta',
+    imageUrl:
+      'https://images.unsplash.com/photo-1598866594230-a7c12756260f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1211&q=80',
+    linkUrl: 'pasta',
+  },
+  {
+    id: 2,
+    title: 'soup',
+    imageUrl:
+      'https://images.unsplash.com/photo-1547592166-23ac45744acd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
+    linkUrl: 'soup',
+  },
+  {
+    id: 3,
+    title: 'salad',
+    imageUrl:
+      'https://images.unsplash.com/photo-1547496502-affa22d38842?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1168&q=80',
+    linkUrl: 'salad',
+  },
+  {
+    id: 4,
+    title: 'sushi',
+    imageUrl:
+      'https://images.unsplash.com/photo-1611143669185-af224c5e3252?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1190&q=80',
+    size: 'large',
+    linkUrl: 'sushi',
+  },
+  {
+    id: 5,
+    title: 'pizza',
+    imageUrl:
+      'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    size: 'large',
+    linkUrl: 'pizza',
+  },
+];
+
+export default sections;
