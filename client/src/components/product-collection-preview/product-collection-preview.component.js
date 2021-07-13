b4c750d1d8fce7b2624dfb86ec6b69342cab5512
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-13 14:28:28 +0300

    changed product-collection-preview component

--- client/src/components/product-collection-preview/product-collection-preview.component.js
+++ client/src/components/product-collection-preview/product-collection-preview.component.js
@@ -1,1 +1,18 @@
-//
+import React from 'react';
+import ProductCollectionItem from 'components/product-collection-item/product-collection-item.component';
+import './product-collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, productCollection }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {productCollection
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <ProductCollectionItem key={item.id} item={item} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
