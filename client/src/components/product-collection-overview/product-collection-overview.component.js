04c8f5b49537347f9b9ab8db6ef8f3d4f5e683e1
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-13 16:31:53 +0300

    changed product-collection-overview

--- client/src/components/product-collection-overview/product-collection-overview.component.js
+++ client/src/components/product-collection-overview/product-collection-overview.component.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './product-collection-overview.styles.scss';
 import ProductCollectionPreview from 'components/product-collection-preview/product-collection-preview.component';
-import productCollections from 'dummies/productCollectionsDummy';
+import productCollectionsDummy from 'dummies/productCollectionsDummy';
 
 const ProductCollectionOverview = () => (
   <div className="collections-overview">
-    {Object.entries(productCollections).map((productCollection, index) => (
+    {Object.entries(productCollectionsDummy).map((productCollection, index) => (
       <ProductCollectionPreview
         title={productCollection[0]}
         productCollection={productCollection[1]}
