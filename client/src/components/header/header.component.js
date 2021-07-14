3e9ff2053461f9c614926a2c3777f800dde8ef20
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-14 17:29:31 +0300

    added cart hiding and items counting functions to header

--- client/src/components/header/header.component.js
+++ client/src/components/header/header.component.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import CartDropDownContainer from 'components/cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from 'redux/cart/cart.selectors';
+import { toggleCartHidden } from 'redux/cart/cart.actions';
+import { selectCartItemsCount } from 'redux/cart/cart.selectors';
 import './header.styles.scss';
 
 const user = 'y';
 
-const Header = () => (
+const Header = ({ cartHidden, toggleCartHidden, itemCount }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <div
@@ -29,16 +34,25 @@ const Header = () => (
           SIGN IN
         </Link>
       )}
-      <div className="cart-icon">
+      <div className="cart-icon" onClick={toggleCartHidden}>
         <div
           className="cart-logo"
           style={{ backgroundImage: `url('/images/cart.svg')` }}
         ></div>
-        <span className="cart-item-count">0</span>
+        <span className="cart-item-count">{itemCount}</span>
       </div>
     </div>
-    <CartDropDownContainer />
+    {!cartHidden ? <CartDropDownContainer /> : null}
   </div>
 );
 
-export default Header;
+const mapStateToProps = createStructuredSelector({
+  cartHidden: selectCartHidden,
+  itemCount: selectCartItemsCount,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
