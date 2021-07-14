f73f08e7da6123e9fc02b06f01f7f0c098dd5b80
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-14 18:39:34 +0300

    added shop to redux

---
+++ client/src/redux/shop/shop.selectors.js
@@ -0,0 +1,25 @@
+import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const selectShop = (state) => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => (collections ? collections[collectionUrlParam] : null)
+    //collections.find(
+    //  (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    //)
+  )
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
+);
