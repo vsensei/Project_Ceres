302d69890f890f6c89f8969c61cf45a0736ee635
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-14 17:26:21 +0300

    started adding redux

---
+++ client/src/redux/cart/cart.types.js
@@ -0,0 +1,9 @@
+const CartActionTypes = {
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM',
+  DELETE_ITEM: 'DELETE_ITEM',
+  DECREASE_ITEM: 'DECREASE_ITEM',
+  CLEAR_CART: 'CLEAR_CART',
+};
+
+export default CartActionTypes;
---
+++ client/src/redux/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import rootReducer from './root-reducer';
+import { persistStore } from 'redux-persist';
+//import createSagaMiddleware from 'redux-saga';
+//import rootSaga from './root-saga';
+
+//const sagaMiddleware = createSagaMiddleware();
+const sharedMiddlewares = []; //[sagaMiddleware];
+const devMiddlewares = [logger];
+
+const middlewares =
+  process.env.NODE_ENV !== 'production'
+    ? [...devMiddlewares, ...sharedMiddlewares]
+    : [...sharedMiddlewares];
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+//sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
