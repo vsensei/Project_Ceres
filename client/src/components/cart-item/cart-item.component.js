a7f932a853c068855a87c7734db02c51befc76ee
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-16 21:23:00 +0300

    changed back cart-item component

--- client/src/components/cart-item/cart-item.component.js
+++ client/src/components/cart-item/cart-item.component.js
@@ -6,9 +6,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <img src={imageUrl} alt="item" />
     <div className="item-details">
       <span className="name">{name}</span>
-      <span className="price">
-        {quantity} x ${price}
-      </span>
+      <span className="price">2 x ${price}</span>
     </div>
   </div>
 );
