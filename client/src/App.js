53d92122da4d5826ad954e2fd0ea479ac296636f
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-16 22:02:43 +0300

    added checkout page to routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from 'components/header/header.component';
 import HomePage from 'pages/homepage/homepage.component';
 import ShopPage from 'pages/shop/shop.component';
+import CheckoutPage from 'pages/checkout/checkout.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
       </Switch>
     </div>
   );
