128b89c156b54ac1ff137abb93ddff00e21dfea2
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-18 04:56:57 +0300

    added signin utils

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -14,6 +14,23 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { userName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({ userName, email, createdAt, ...additionalData });
+    } catch (e) {
+      console.log('error creating user', e.message);
+    }
+  }
+  return userRef;
+};
+
 export const addCollectionAndDocuments = async (
   CollectionKey,
   objectsToAdd
