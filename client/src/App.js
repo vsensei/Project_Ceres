3f83b64a024d7b127ba800270c9a00003f7ad483
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-18 04:24:20 +0300

    added signin and signup routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Header from 'components/header/header.component';
 import HomePage from 'pages/homepage/homepage.component';
 import ShopPage from 'pages/shop/shop.component';
 import CheckoutPage from 'pages/checkout/checkout.component';
+import SignUpPage from 'pages/signup/signup.component';
+import SignInPage from 'pages/signin/signin.component';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
         <Route exact path="/checkout" component={CheckoutPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+        <Route exact path="/signin" component={SignInPage} />
       </Switch>
     </div>
   );
