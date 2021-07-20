f1ea9cb87ce1cd82b3acf3d14046763cf8d5fa61
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-20 06:50:44 +0300

    added spinner and with-spinner

---
+++ client/src/components/shared/with-spinner/spinner/spinner.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './spinner.styles.scss';
+
+const Spinner = () => (
+  <div className="spinner-overlay">
+    <div className="spinner"></div>
+  </div>
+);
+
+export default Spinner;
