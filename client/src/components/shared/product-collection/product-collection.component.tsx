08dfc74ee6f3373f6f4bc9964efc76cc87825dce
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-30 03:05:06 +0300

    started converting product-collection component

--- client/src/components/shared/product-collection/product-collection.component.tsx
+++ client/src/components/shared/product-collection/product-collection.component.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import ProductCollectionItem from 'components/product-collection-item/product-collection-item.component';
 import './product-collection.styles.scss';
 
-const ProductCollection = ({ title, productCollection, subClass }) => (
+type Item = {
+  id: number;
+};
+
+type Props = {
+  title: string;
+  productCollection: Array<Item>;
+  subClass: string;
+};
+
+const ProductCollection: React.FC<Props> = ({
+  title,
+  productCollection,
+  subClass,
+}) => (
   <div className="product-collection-container">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className={`product-collection${subClass ? ' ' + subClass : ''}`}>
