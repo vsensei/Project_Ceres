5164350e640295109f3b1753a6b69db5936ea7a3
VinishkoSensei <fatefulreaper@gmail.com>
2021-07-31 05:01:59 +0300

    changed directory-item component

--- client/src/components/directory-item/directory-item.component.tsx
+++ client/src/components/directory-item/directory-item.component.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './directory-item.styles.scss';
 import { withRouter } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router';
 
-const DirectoryItem = ({
+type PathParamsType = {};
+
+type Props = RouteComponentProps<PathParamsType> & {
+  title?: string;
+  imageUrl?: string;
+  size?: number;
+  linkUrl?: string;
+  match: any;
+  chooseDirectoryItem: Function;
+};
+
+const DirectoryItem: React.FC<Props> = ({
   title,
   imageUrl,
   size,
@@ -19,7 +31,7 @@ const DirectoryItem = ({
       style={{ backgroundImage: `url(${imageUrl})` }}
     ></div>
     <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <h1 className="title">{title ? title.toUpperCase() : null}</h1>
       <span className="subtitle">BUY NOW</span>
     </div>
   </div>
